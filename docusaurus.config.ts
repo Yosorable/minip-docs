75158293016cba16c3a7a1f38524cc998476eb40
Yosorable <1354464666@qq.com>
2025-02-15 15:36:21 +0800

    fix editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/Yosorable/minip-docs",
+          editUrl: "https://github.com/Yosorable/minip-docs/tree/main",
         },
         theme: {
           customCss: "./src/css/custom.css",
